8db5b30db1a114a5f853ae0d34239331f8310741
Jiamu Sun <barroit@linux.com>
2025-08-20 22:08:40 +0900

    rework: split image init and render
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- core/render.js
+++ core/render.js
@@ -6,13 +6,16 @@
 const scale = window.devicePixelRatio
 
 const canvas_box = document.getElementById('canvas-box')
-const canvas = canvas_box.firstElementChild
+const canvas = document.getElementById('canvas')
 const buffer = document.getElementById('buffer')
 
 let dumpline
 let block
 
-function render(event)
+let binary
+let blob
+
+async function image_render(event, done)
 {
 	const img = event.target
 	const ctx = buffer.getContext('2d')
@@ -20,15 +23,13 @@ function render(event)
 	ctx.setTransform(scale, 0, 0, scale, 0, 0)
 	ctx.drawImage(img, 0, 0)
 
-	buffer.toBlob(async blob =>
-	{
-		const data = await blob.arrayBuffer()
+	blob = await new Promise(ret => buffer.toBlob(ret))
+	binary = await blob.arrayBuffer()
 
-		dumpline.postMessage({ data })
-	})
+	done()
 }
 
-function dump(event)
+function image_init(event)
 {
 	const trans = event.clipboardData
 	const data = trans.getData('text/html')
@@ -47,18 +48,29 @@ function dump(event)
 	buffer.width = w * scale
 	buffer.height = h * scale
 
+	return clone
+}
+
+async function image_save(event)
+{
+	const xml = image_init(event)
 	const fmter = new XMLSerializer()
-	const str = fmter.serializeToString(clone)
+	const str = fmter.serializeToString(xml)
 
 	const uri = encodeURIComponent(str)
 	const b64 = `data:image/svg+xml;charset=utf-8,${ uri }`
 	const img = new Image()
 
-	img.src = b64
-	img.onload = render
+	await new Promise(done =>
+	{
+		img.src = b64
+		img.onload = (event) => image_render(event, done)
+	})
+
+	dumpline.postMessage({ binary })
 }
 
-document.addEventListener('paste', dump)
+document.addEventListener('paste', image_save)
 
 window.addEventListener('message', event =>
 {
