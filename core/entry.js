4fb5700ef1975cc7039994ca341cd068e5b5ba95
Jiamu Sun <barroit@linux.com>
2025-08-22 23:55:14 +0900

    dump: handle plaintext block
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- core/entry.js
+++ core/entry.js
@@ -3,11 +3,10 @@
  * Copyright 2025 Jiamu Sun <barroit@linux.com>
  */
 
-import { writeFileSync, mkdirSync, readdirSync } from 'node:fs'
+import { writeFileSync, mkdirSync } from 'node:fs'
 import { platform } from 'node:process'
 import { commands, window, workspace, ViewColumn, Uri } from 'vscode'
 
-import { cfg_read, cfg_write_p } from './config.js'
 import panel_html from './panel.js'
 
 const { executeCommand, registerTextEditorCommand } = commands
@@ -20,47 +19,6 @@ let panel
 let root
 let tmpdir
 let current
-
-const macos = platform.name == 'darwin'
-
-/*
- * No API? Fine. We just went ahead and built the damn thing ourselves.
- *
- * src/vs/editor/common/config/editorOptions.ts:EDITOR_FONT_DEFAULTS
- * src/vs/editor/common/config/fontInfo.ts:_create()
- */
-const FONT_SIZE = macos ? 12 : 14
-const LINE_HEIGHT = 0
-const LINE_HEIGHT_RATIO = macos ? 1.5 : 1.35
-const LINE_HEIGHT_MIN = 8
-
-function line_height_calc(h, scale)
-{
-	if (h == 0)
-		h = LINE_HEIGHT_RATIO * scale
-	else if (h < LINE_HEIGHT_MIN)
-		h *= scale
-
-	h = Math.round(h)
-	return h < LINE_HEIGHT_MIN ? LINE_HEIGHT_MIN : h
-}
-
-function sanitize_config(cfg)
-{
-	let font_size = cfg_read(cfg, 'editor', 'fontSize')
-	let line_height = cfg_read(cfg, 'editor', 'lineHeight')
-
-	if (!font_size) {
-		font_size = FONT_SIZE
-		cfg_write_p(cfg, 'editor', 'fontSize', font_size)
-	}
-
-	if (!line_height)
-		line_height = LINE_HEIGHT
-
-	line_height = line_height_calc(line_height, font_size)
-	cfg_write_p(cfg, 'editor', 'lineHeight', line_height)
-}
 
 function save_image({ binary })
 {
@@ -112,8 +70,6 @@ async function dump_handler()
 
 	current.config = JSON.stringify(current.config)
 	current.config = JSON.parse(current.config)
-
-	sanitize_config(current.config)
 
 	if (panel)
 		panel.reveal(panel.viewColumn, true)
