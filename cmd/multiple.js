48fc356c4d1bc21f776bd2a3cb3327063557a187
Jiamu Sun <barroit@linux.com>
2025-12-30 22:28:48 +0900

    cmd: add preview panel
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- cmd/multiple.js
+++ cmd/multiple.js
@@ -3,6 +3,78 @@
  * Copyright 2025 Jiamu Sun <barroit@linux.com>
  */
 
-export function exec(editor)
+import { platform } from 'node:process'
+
+import { die } from '../helper/mesg.js'
+import { vsc_range, vsc_env, vsc_exec_cmd } from '../helper/vsc.js'
+
+import { opt_ensure_valid } from '../helper.patch/option.js'
+import { panel_init } from '../helper.patch/panel.js'
+
+const cp_rich_cmd = 'editor.action.clipboardCopyWithSyntaxHighlightingAction'
+
+let panel
+
+function map_ctx(editor, ctx_in)
 {
+	const select = editor.selection
+	const ctx = {
+		...ctx_in,
+		platform,
+
+		begin_row: select.start.line,
+		begin_col: select.start.character,
+
+		end_row: select.end.line,
+		end_col: select.end.character,
+
+		tabstop: editor.options.tabSize,
+	}
+
+	return ctx
+}
+
+function dump_binary()
+{
+	//
+}
+
+function reset_panel()
+{
+	panel = undefined
+}
+
+function recv_mesg(event)
+{
+	const [ name, data ] = event
+
+	switch (name) {
+	case 'open':
+		cb = vsc_env.openExternal
+		break
+	case 'dump':
+		cb = dump_binary
+	}
+
+	cb(data)
+}
+
+export async function exec(editor)
+{
+	if (editor.selections.length > 1)
+		die("multiple selections aren't supported")
+
+	if (panel)
+		panel.reveal(panel.viewColumn, true)
+	else
+		panel = panel_init(this, recv_mesg, reset_panel)
+
+	const opt = this.fetch_config()
+
+	opt_ensure_valid(opt)
+
+	const ctx = map_ctx(editor, opt)
+
+	await vsc_exec_cmd(cp_rich_cmd)
+	panel.webview.postMessage(ctx)
 }
--- patch.mak
+++ patch.mak
@@ -1,11 +1,23 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-packages  := @slimio/wcwidth pngjs
+packages  := @slimio/wcwidth pngjs tailwindcss
 modules   := $(addprefix $(module-prefix)/,$(packages))
 modules-y := $(addsuffix /package.json,$(modules))
 
-prebundle := $(modules-y)
+helper-in := $(wildcard helper.patch/*.js.in)
+helper-y  := $(helper-in:%.in=%)
+
+panel-in := $(wildcard panel/*.html)
+panel-y  := $(prefix)/index.html
+
+prebundle := $(modules-y) $(helper-y)
 
 $(modules-y): $(module-prefix)/%/package.json:
-	$(npm-install) $*
+	$(npm) $*
 	touch $(package).in
+
+$(panel-y): $(panel-in) $(prefix)
+	$(m4) $< >$@
+
+$(helper-y): %: %.in $(panel-y)
+	$(m4) $< >$@
